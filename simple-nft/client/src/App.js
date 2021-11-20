dea262b53ee21c71c5cfa7ed8bdad6f57f19067a
Vib-UX <vibchess123@gmail.com>
2021-11-20 21:19:25 +0530

    NFT_FE

--- simple-nft/client/src/App.js
+++ simple-nft/client/src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 
 
 class App extends Component {
-  state = {loaded: false, tokenSaleAddress: null, userTokens: 0};
+  state = {loaded: false};
   
   componentDidMount = async () => {
     try {
@@ -23,27 +23,27 @@ class App extends Component {
       // Get the contract instance.
       this.networkId = await this.web3.eth.net.getId();
       
-      this.tokenInstance = new this.web3.eth.Contract(
-        CDEXToken.abi,
-        CDEXToken.networks[this.networkId] && CDEXToken.networks[this.networkId].address,
+      this.NFT = new this.web3.eth.Contract(
+        SimpleNFT.abi,
+        SimpleNFT.networks[this.networkId] && SimpleNFT.networks[this.networkId].address,
       );
       
-
-      this.tokenSaleInstance = new this.web3.eth.Contract(
-        CDEXSale.abi,
-        CDEXSale.networks[this.networkId] && CDEXSale.networks[this.networkId].address,
-      );
+      // we will deal with it later
+      // this.tokenSaleInstance = new this.web3.eth.Contract(
+      //   CDEXSale.abi,
+      //   CDEXSale.networks[this.networkId] && CDEXSale.networks[this.networkId].address,
+      // );
       
 
-      this.kycInstance = new this.web3.eth.Contract(
-        KycContract.abi,
-        KycContract.networks[this.networkId] && KycContract.networks[this.networkId].address,
-      );
+      // this.kycInstance = new this.web3.eth.Contract(
+      //   KycContract.abi,
+      //   KycContract.networks[this.networkId] && KycContract.networks[this.networkId].address,
+      // );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.listenToTokenTransfer();
-      this.setState({loaded: true, tokenSaleAddress: CDEXSale.networks[this.networkId].address}, this.updateUserTokens);
+      // this.listenToTokenTransfer();
+      this.setState({loaded: true});
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -53,17 +53,18 @@ class App extends Component {
     }
   };
 
-  updateUserTokens = async ()=>{
-    let userTokens = await this.tokenInstance.methods.balanceOf(this.accounts[0]).call();
-    this.setState({userTokens: userTokens});
-  }
+  // updateUserTokens = async ()=>{
+  //   let userTokens = await this.tokenInstance.methods.balanceOf(this.accounts[0]).call();
+  //   this.setState({userTokens: userTokens});
+  // }
 
-  listenToTokenTransfer = () =>{
-    this.tokenInstance.events.Transfer({to: this.accounts[0]}).on("data", this.updateUserTokens);
-  }
+  // listenToTokenTransfer = () =>{
+  //   this.tokenInstance.events.Transfer({to: this.accounts[0]}).on("data", this.updateUserTokens);
+  // }
 
-  handleBuyTokens = async() =>{
-    await this.tokenSaleInstance.methods.buyTokens(this.accounts[0]).send({from: this.accounts[0], value: this.web3.utils.toWei("1","wei")});
+  handlemintNft = async() =>{
+    await this.NFT.methods.mintNft("nft_supernova",100,this.accounts[0]).send({from: this.accounts[0], value: 0});
+    // console.log(this.NFT.methods._listed(0).call())
   }
 
   handleInputChange = (event)=>{
@@ -75,10 +76,10 @@ class App extends Component {
     })
   };
 
-  handleKycWhitelisting = async ()=>{
-    await this.kycInstance.methods.setKycCompleted(this.state.kycAddress).send({from: this.accounts[0]});
-    alert("Kyc "+this.state.kycAddress+" is completed");
-  }
+  // handleKycWhitelisting = async ()=>{
+  //   await this.kycInstance.methods.setKycCompleted(this.state.kycAddress).send({from: this.accounts[0]});
+  //   alert("Kyc "+this.state.kycAddress+" is completed");
+  // }
 
   render() {
     if (!this.state.loaded) {
@@ -86,15 +87,17 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>CDEX Token Sale</h1>
-        <p>Get your Tokens today!.</p>
-        <h2>Kyc Whitelisting</h2>
+        <h1>Welcome to NFT Mint</h1>
+        <p>Get your NFT today!.</p>
+
+        {/* <h2>Kyc Whitelisting</h2>
         Address to allow: <input type="text" name="kycAddress" value={this.state.kycAddress} onChange={this.handleInputChange}/>
         <button type="button" onClick={this.handleKycWhitelisting}>Add to Whitelist</button>
         <h2>Buy CDEX Tokens </h2>
         <p>If you want to buy tokens, send Wei to this address: {this.state.tokenSaleAddress}</p>
         <p>You curretly have: {this.state.userTokens} CDEX Tokens</p>
-        <button type="button" onClick={this.handleBuyTokens}>Buy more tokens</button>
+         */}
+        <button type="button" onClick={this.handlemintNft}>Mint more NFT</button>
       </div>
     );
   }
